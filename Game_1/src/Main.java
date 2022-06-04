1babcec4776a0ac0a7efabb0461e37e746f950f5
KaustubhSiriki <kaustubh.siriki@gmail.com>
2022-06-04 18:05:27 -0400

    update

---
+++ Game_1/src/Main.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.println("Hi, what's your name?");
+        String name = keyboard.nextLine();
+        System.out.println("Hey " + name + ", wanna join me in a game of rock, paper, scissors, lizard, and spock? \n- Play \n- Exit");
+        String menu_option = keyboard.nextLine();
+        String [] plays = {"Rock", "Paper", "Scissors", "Lizard", "Spock"};
+        String computer = plays[(int)(Math.random() * 5)];
+
+        while (menu_option.equalsIgnoreCase("play")){
+            System.out.println("You'll play the computer!");
+            System.out.println("Select an option: \n- Rock \n- Paper \n- Scissors \n- Lizard \n- Spock");
+            String player = keyboard.nextLine();
+
+            while (!(player.equalsIgnoreCase("Rock") || player.equalsIgnoreCase("Paper") || player.equalsIgnoreCase("Scissors") || player.equalsIgnoreCase("Lizard") || player.equalsIgnoreCase("Spock"))){
+                System.out.println("ERROR: Please type in an option!");
+                player = keyboard.nextLine();
+            }
+
+            System.out.println("Computer: " + computer + "\nYou: " + player);
+
+            if (player.equalsIgnoreCase("Rock") && (computer.equalsIgnoreCase("Paper") || computer.equalsIgnoreCase("Spock"))) {
+                System.out.println("You Lose!");
+            } else if (player.equalsIgnoreCase("Rock") && (computer.equalsIgnoreCase("Scissors") || computer.equalsIgnoreCase("Lizard"))) {
+                System.out.println("You Win!");
+            } else {
+                System.out.println("You Tied!");
+            }
+
+            if (player.equalsIgnoreCase("Paper") && (computer.equalsIgnoreCase("Paper") || computer.equalsIgnoreCase("Spock"))) {
+                System.out.println("You Lose!");
+            } else if (player.equalsIgnoreCase("Paper") && (computer.equalsIgnoreCase("Scissors") || computer.equalsIgnoreCase("Lizard"))) {
+                System.out.println("You Win!");
+            } else {
+                System.out.println("You Tied!");
+            }
+
+            menu_option = "none";
+
+        }
+    }
+}
--- HelloWorld/src/helloWorld/HelloWorld.java
+++ HelloWorld/src/helloWorld/HelloWorld.java
@@ -1,5 +1,68 @@
 package helloWorld;
 
-public class HelloWorld {
+import java.util.Scanner;
 
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        Scanner keyboard = new Scanner(System.in);
+        String playAgain = "y";
+        while (playAgain.equals("y")) {
+            System.out.println("Enter your move. Pick rock, paper, scissors, lizard, or spock: ");
+
+
+            //(validation loop) asking player for their move and making sure it is valid
+            String playerChoice = "";
+            playerChoice = keyboard.nextLine();
+            while (!playerChoice.equalsIgnoreCase("rock") &&
+                    !playerChoice.equalsIgnoreCase("paper") &&
+                    !playerChoice.equalsIgnoreCase("scissors") &&
+                    !playerChoice.equalsIgnoreCase("lizard") &&
+                    !playerChoice.equalsIgnoreCase("spock")) {
+                playerChoice = keyboard.nextLine();
+                System.out.println("Enter your move. Pick rock, paper, scissors, lizard, or spock: ");
+            }
+
+            int randomNumber1To5 = (int) (Math.random() * 2 + 1);
+            String computerChoice = "";
+            if (randomNumber1To5 == 1) {
+                computerChoice = "rock";
+            } else if (randomNumber1To5 == 2) {
+                computerChoice = "paper";
+            } else if (randomNumber1To5 == 3) {
+                computerChoice = "scissors";
+            } else if (randomNumber1To5 == 4) {
+                computerChoice = "lizard";
+            } else {
+                computerChoice = "spock";
+            }
+
+            System.out.println("Player's move: " + playerChoice);
+            System.out.println("Computer's move: " + computerChoice);
+
+            if (playerChoice.equals(computerChoice)) {
+                System.out.println("It is a tie!");
+            } else if ((playerChoice.equals("rock")) && (computerChoice.equals("scissors")) ||
+                    playerChoice.equals("rock") && computerChoice.equals("lizard") ||
+                    playerChoice.equals("paper") && computerChoice.equals("rock") ||
+                    playerChoice.equals("paper") && computerChoice.equals("spock") ||
+                    playerChoice.equals("scissors") && computerChoice.equals("paper") ||
+                    playerChoice.equals("scissors") && computerChoice.equals("lizard") ||
+                    playerChoice.equals("lizard") && computerChoice.equals("paper") ||
+                    playerChoice.equals("lizard") && computerChoice.equals("spock") ||
+                    playerChoice.equals("spock") && computerChoice.equals("rock") ||
+                    playerChoice.equals("Spock") && computerChoice.equals("scissors")) {
+                System.out.println("You win!");
+            } else {
+                System.out.println("Sorry, you lose.");
+            }
+            playAgain = "n";
+
+            while (playAgain.equalsIgnoreCase("n")) {
+                System.out.println("Do you want to play again? (y/n)");
+                playAgain = keyboard.nextLine();
+            }
+            }
+
+    }
 }
